1b1925bbc26189f4e7584db55742e1f0b52a39d3
ZPYin <528172875@qq.com>
2019-01-08 22:36:13 +0100

    replace curl with wget
    Former-commit-id: 27fdaf3990699106695ae81e27cbb734c035cc43

--- lib/download_radiosonde_list.m
+++ lib/download_radiosonde_list.m
@@ -15,13 +15,20 @@ url = 'https://www1.ncdc.noaa.gov/pub/data/igra/igra2-station-list.txt';
 saveFile = 'radiosonde-station-list.txt';
 saveFolder = fullfile(parentFolder(mfilename('fullpath'), 2), 'doc');
 
-status = system(sprintf('curl -o %s %s', fullfile(saveFolder, saveFile), url));
+if ispc
+    status = system(sprintf('curl -o %s %s', fullfile(saveFolder, saveFile), url));
+    if status ~= 0
+        error('Error in calling curl in window cmd. Please make sure curl is in the searching path.');
+    end
+elseif isunix
+    status = system(sprintf('wget -qO %s %s', fullfile(saveFolder, saveFile), url));
+end
 
 if status == 0
     flag = true;
     fprintf('Updating the radiosonde-station-list successfully!\n');
 else
-    fprintf('Failure in updating the radiosonde-station-list.\nTry to debug with %s\n', sprintf('curl -o %s %s', fullfile(saveFolder, saveFile), url));
+    fprintf('Failure in updating the radiosonde-station-list.\nTry to debug with %s\n', sprintf('wget -qO %s %s', fullfile(saveFolder, saveFile), url));
     flag = false;
 end
 
