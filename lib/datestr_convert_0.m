03565f95fc7f91bcde9b601cf2dcfe3ce60b71d5
ZPYin <528172875@qq.com>
2019-05-15 08:09:47 +0200

    fix the bug of gdas timestamp

---
+++ lib/datestr_convert_0.m
@@ -0,0 +1,28 @@
+function [thisStr] = datestr_convert_0(thisDatenum, thisFormat)
+%datestr_convert_0 Convert datenum to datestr with keeping 0 to empty string, instead of '00000100'
+%   Example:
+%       [thisStr] = datestr_convert_0(thisDatenum)
+%   Inputs:
+%       thisDatenum: float
+%           matlab datenum.
+%   Outputs:
+%       thisStr: char array
+%           date string corresponding to the input matlab datenum.
+%       thisFormat: char array
+%           date string format with being compatible with datestr format.
+%   History:
+%       2019-05-15. First Edition by Zhenping
+%   Contact:
+%       zhenping@tropos.de
+
+if ~ exist('thisFormat', 'var')
+    thisFormat = 'yyyymmdd HH:MM:SS';
+end
+
+if abs(thisDatenum) <= 1e-16
+    thisStr = '';
+else
+    thisStr = datestr(thisDatenum, thisFormat);
+end
+
+end
