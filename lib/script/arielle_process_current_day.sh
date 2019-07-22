330e271ddfd39626ee58af2e3bd5db314627e5e9
ZPYin <528172875@qq.com>
2019-07-22 23:11:50 +0200

    add shell scripts to auto process polly data
    Former-commit-id: 352582ef87bd5fc5799710bb40b3b6af7c4212fc
    Former-commit-id: eeaec7a4ec97ac5468dbc4340e51e94c2d52b925

---
+++ lib/script/arielle_process_current_day.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This script will help to process the current polly data with using Pollynet processing chain
+
+# parameter initialization
+POLLY_FOLDER="/oceanethome/pollyxt"
+POLLY_TYPE="arielle"
+TODOLISTFOLDER="/home/picasso/Pollynet_Processing_Chain/todo_filelist"
+
+echo "\nCurrent time: "
+date +"%Y-%m-%d"
+
+a=$(date +"%Y-%m-%d")
+year=$(echo $a | cut -b1-4)
+month=$(echo $a | cut -b6-7)
+day=$(echo $a | cut -b9-10)
+
+echo -e "\nInitial settings:\nPOLLY_FOLDER=$POLLY_FOLDER\nPOLLY_TYPE=$POLLY_TYPE\nTODOLISTFOLDER=$TODOLISTFOLDER\n\n"
+
+matlab -nodisplay -nodesktop -nosplash << ENDMATLAB
+
+clc;
+
+write_daily_to_filelist($POLLY_TYPE, $POLLY_FOLDER, $TODOLISTFOLDER, $year, $month, $day, 'w');
+pollynet_processing_chain_main;
+
+exit;
+
+ENDMATLAB
+
+echo "Finish"
