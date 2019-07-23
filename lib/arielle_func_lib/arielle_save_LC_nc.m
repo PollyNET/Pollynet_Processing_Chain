fea39d7d0ee5b5e302bd4cf3972586ef2a2bb1bf
ZPYin <528172875@qq.com>
2019-07-23 10:31:53 +0200

    write git info to nc file
    Former-commit-id: 82823fd8eab12009378326955239b7ac9777b648
    Former-commit-id: 650d08d50ef7a8476f938318e4bd362b4d5fd094

--- lib/arielle_func_lib/arielle_save_LC_nc.m
+++ lib/arielle_func_lib/arielle_save_LC_nc.m
@@ -413,7 +413,8 @@ netcdf.putAtt(ncID, varID_global, 'source', campaignInfo.name);
 netcdf.putAtt(ncID, varID_global, 'version', processInfo.programVersion);
 netcdf.putAtt(ncID, varID_global, 'reference', processInfo.homepage);
 netcdf.putAtt(ncID, varID_global, 'contact', processInfo.contact);
-netcdf.putAtt(ncID, varID_global, 'history', sprintf('Last processing time at %s by %s', tNow, mfilename));
+gitInfo = getGitInfo();
+netcdf.putAtt(ncID, varID_global, 'history', sprintf('Last processing time at %s by %s, git branch: %s, git commit: %s', tNow, mfilename, gitInfo.branch, gitInfo.commit));
     
 % close file
 netcdf.close(ncID);
