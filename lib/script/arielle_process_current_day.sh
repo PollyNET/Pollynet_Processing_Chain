e72c0a26ead39eb5644f209d29815048a5ffe729
zhenping <zhenping@tropos.de>
2019-07-28 10:41:35 +0000

    fix the bug in write_single_to_filelist.m
    Former-commit-id: ac58c8c5e208fcb2e31c78be51b959f0deff70a9
    Former-commit-id: 23ddc2de867b72a0657cd6224500c699e2b1c1c0

--- lib/script/arielle_process_current_day.sh
+++ lib/script/arielle_process_current_day.sh
@@ -14,13 +14,13 @@ year=$(echo $a | cut -b1-4)
 month=$(echo $a | cut -b6-7)
 day=$(echo $a | cut -b9-10)
 
-echo -e "\nInitial settings:\nPOLLY_FOLDER=$POLLY_FOLDER\nPOLLY_TYPE=$POLLY_TYPE\nTODOLISTFOLDER=$TODOLISTFOLDER\n\n"
+echo -e "\nInitial settings:\nPOLLY_FOLDER=$POLLY_FOLDER\nPOLLY_TYPE=$POLLY_TYPE\nTODOLISTFOLDER=$TODOLISTFOLDER\nYear=$year\nmonth=$month\nday=$day\n\n"
 
 matlab -nodisplay -nodesktop -nosplash << ENDMATLAB
 
 clc;
 
-write_daily_to_filelist('$POLLY_TYPE', '$POLLY_FOLDER', '$TODOLISTFOLDER', $year, $month, $day, 'w');
+write_daily_to_filelist('$POLLY_TYPE', '$POLLY_FOLDER', '$TODOLISTFOLDER', '$year', '$month', '$day', 'w');
 pollynet_processing_chain_main;
 
 exit;
