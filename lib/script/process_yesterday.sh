cd1fead37ac9067c7d3812d54191019ccc177e2f
Zhenping <zhenping@tropos.de>
2019-08-07 08:44:16 +0000

    update the process_yesterday
    Former-commit-id: e021f9dc6d5acbfeb57309bbe80d4c6157da6b44
    Former-commit-id: 7f78bc84918873af6bc6e68fd628192d869a8288

--- lib/script/process_yesterday.sh
+++ lib/script/process_yesterday.sh
@@ -8,12 +8,16 @@ PATH=${PATH}:/usr/programming/matlab/matlab-2014a/bin
 
 echo "Process the current available polly data"
 YYYYMMDD=$(date --utc "+%Y%m%d" -d "yesterday")
+pollyRoot="/pollyhome"
 
 Year=$(echo ${YYYYMMDD} | cut -c1-4)
 Month=$(echo ${YYYYMMDD} | cut -c5-6)
 Day=$(echo ${YYYYMMDD} | cut -c7-8)
 
 echo "Processing $YYYYMMDD"
+echo "Year=$Year"
+echo "Month=$Month"
+echo "Day=$Day"
 
 # parameter definition
 pollyList="'arielle','pollyxt_lacros','polly_1v2','pollyxt_fmi','pollyxt_dwd','pollyxt_noa','pollyxt_tropos','pollyxt_uw','pollyxt_tjk'"
@@ -23,7 +27,9 @@ cd /pollyhome/Picasso/playground;
 pollyList = {${pollyList}};
 
 for iPolly = 1:length(pollyList)
-	pollynet_process_history_data(pollyList{iPolly}, datenum(${Year}, ${Month}, ${Day}), datenum(${Year}, ${Month}, ${Day}));
+	saveFolder = fullfile('$pollyRoot', pollyList{iPolly});
+	todoFolder = '/pollyhome/Picasso/todo_filelist';
+	pollynet_process_history_data(pollyList{iPolly}, '$YYYYMMDD', '$YYYYMMDD', saveFolder, todoFolder);
 end
 ENDMATLAB
 
