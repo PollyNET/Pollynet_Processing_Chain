84989e59a816a86a8d13e4400bf07205c599df5c
ZPYin <528172875@qq.com>
2019-08-19 20:58:42 +0200

    Enhancement: add quasi_bsc_355

--- lib/pollyxt_lacros_func_lib/pollyxt_lacros_display_quasiretrieving.py
+++ lib/pollyxt_lacros_func_lib/pollyxt_lacros_display_quasiretrieving.py
@@ -76,12 +76,15 @@ def pollyxt_lacros_display_quasiretrieving(tmpFile, saveFolder):
         figDPI = mat['figDPI'][0][0]
         quasi_bsc_532 = mat['quasi_bsc_532'][:]
         quality_mask_532 = mat['quality_mask_532'][:]
+        quasi_bsc_355 = mat['quasi_bsc_355'][:]
+        quality_mask_355 = mat['quality_mask_355'][:]
         quasi_bsc_1064 = mat['quasi_bsc_1064'][:]
         quality_mask_1064 = mat['quality_mask_1064'][:]
         quasi_pardepol_532 = mat['quasi_pardepol_532'][:]
         quasi_ang_532_1064 = mat['quasi_ang_532_1064'][:]
         height = mat['height'][0][:]
         time = mat['time'][0][:]
+        quasi_beta_cRange_355 = mat['quasi_beta_cRange_355'][0][:]
         quasi_beta_cRange_532 = mat['quasi_beta_cRange_532'][0][:]
         quasi_beta_cRange_1064 = mat['quasi_beta_cRange_1064'][0][:]
         quasi_Par_DR_cRange_532 = mat['quasi_Par_DR_cRange_532'][0][:]
@@ -97,6 +100,7 @@ def pollyxt_lacros_display_quasiretrieving(tmpFile, saveFolder):
 
     # meshgrid
     Time, Height = np.meshgrid(time, height)
+    quasi_bsc_355 = np.ma.masked_where(quality_mask_355 > 0, quasi_bsc_355)
     quasi_bsc_532 = np.ma.masked_where(quality_mask_532 > 0, quasi_bsc_532)
     quasi_bsc_1064 = np.ma.masked_where(quality_mask_1064 > 0, quasi_bsc_1064)
     quasi_pardepol_532 = np.ma.masked_where(quality_mask_532 > 0, quasi_pardepol_532)
@@ -107,6 +111,35 @@ def pollyxt_lacros_display_quasiretrieving(tmpFile, saveFolder):
     cmap.set_bad('w', alpha=1)
     cmap.set_over('w', alpha=1)
     cmap.set_under('k', alpha=1)
+
+    # display quasi backscatter at 355 nm
+    fig = plt.figure(figsize=[10, 5])
+    ax = fig.add_axes([0.1, 0.15, 0.8, 0.75])
+    pcmesh = ax.pcolormesh(Time, Height, quasi_bsc_355 * 1e6, vmin=quasi_beta_cRange_355[0], vmax=quasi_beta_cRange_355[1], cmap=cmap)
+    ax.set_xlabel('UTC', fontsize=15)
+    ax.set_ylabel('Height (m)', fontsize=15)
+
+    ax.yaxis.set_major_locator(MultipleLocator(2000))
+    ax.yaxis.set_minor_locator(MultipleLocator(500))
+    ax.set_ylim([0, 12000])
+    ax.set_xticks(xtick.tolist())
+    ax.set_xticklabels(celltolist(xticklabel))
+    ax.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
+    ax.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+
+    ax.set_title('Quasi backscatter coefficient at {wave}nm from {instrument} at {location}'.format(wave=355, instrument=pollyVersion, location=location), fontsize=15)
+
+    cb_ax = fig.add_axes([0.92, 0.20, 0.02, 0.65])
+    cbar = fig.colorbar(pcmesh, cax=cb_ax, ticks=np.linspace(quasi_beta_cRange_355[0], quasi_beta_cRange_355[1], 5), orientation='vertical')
+    cbar.ax.tick_params(direction='in', labelsize=15, pad=5)
+    cbar.ax.set_title('[$Mm^{-1}*Sr^{-1}$]', fontsize=12)
+
+    fig.text(0.05, 0.02, datenum_to_datetime(time[0]).strftime("%Y-%m-%d"), fontsize=15)
+    fig.text(0.8, 0.02, 'Version: {version}'.format(version=version), fontsize=15)
+
+    
+    fig.savefig(os.path.join(saveFolder, '{dataFilename}_Quasi_Bsc_355.png'.format(dataFilename=rmext(dataFilename))), dpi=figDPI)
+    plt.close()
 
     # display quasi backscatter at 532 nm
     fig = plt.figure(figsize=[10, 5])
