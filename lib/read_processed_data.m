32f427258629fc3b88e6d75f99859b0599190b74
ZPYin <528172875@qq.com>
2019-08-28 11:26:33 +0200

    Improvement: update the read_processed_data.m
    Former-commit-id: 787754d4894851f86bc37d1baacf5390c9e9e59a
    Former-commit-id: 3e42a963574c72e53cc0dbe4d9d5a9fd58161a54
    Former-commit-id: 536e70fad6ac3995317fb6a35fbf662de7a6619e

--- lib/read_processed_data.m
+++ lib/read_processed_data.m
@@ -324,6 +324,398 @@ case 'attenuated_backscatter_532nm'
     height = height(hIndx);
     time = time(indx);
 
+case 'quality_mask_1064'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quality_mask_1064');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quality_mask_532'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quality_mask_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quality_mask_voldepol_532'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quality_mask_voldepol_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_ang_532_1064'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_ang_532_1064');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_bsc_1064'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_bsc_1064');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_bsc_532'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_bsc_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_pardepol_532'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_pardepol_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quality_mask_1064_V2'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results_V2.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quality_mask_1064');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quality_mask_532_V2'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results_V2.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quality_mask_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quality_mask_voldepol_532_V2'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results_V2.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quality_mask_voldepol_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_ang_532_1064_V2'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results_V2.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_ang_532_1064');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_bsc_1064_V2'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results_V2.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_bsc_1064');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_bsc_532_V2'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results_V2.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_bsc_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
+case 'quasi_pardepol_532_V2'
+    
+    for iDay = fix(tRange(1)):fix(tRange(2))
+        files = listfile(fullfile(resFolder, instrument, ...
+        datestr(double(iDay), 'yyyy'), ...
+        datestr(double(iDay), 'mm'), ...
+        datestr(double(iDay), 'dd')), '\w*_quasi_results_V2.nc');
+        for iFile = 1:length(files)
+            fprintf('Reading %s.\n', files{iFile});
+            time_raw = unix_timestamp_2_datenum(ncread(files{iFile}, 'time'));
+            height_raw = ncread(files{iFile}, 'height');
+            res_raw = ncread(files{iFile}, 'quasi_pardepol_532');
+
+            % partition the array
+            tIndx = (time_raw >= tRange(1)) & (time_raw <= tRange(2));
+            hIndx = (height_raw/1e3 >= hRange(1)) & (height_raw/1e3 <= hRange(2));
+            time = [time; time_raw(tIndx)];
+            height = height_raw(hIndx);
+            res = [res, res_raw(hIndx, tIndx)];
+        end
+    end
+    
+    indx = (time >= tRange(1)) & (time <= tRange(2));
+    hIndx = (height/1e3 >= hRange(1)) & (height/1e3 <= hRange(2));
+    res = res(hIndx, indx);
+    height = height(hIndx);
+    time = time(indx);
+
 case 'target_classification'
     
     for iDay = fix(tRange(1)):fix(tRange(2))
