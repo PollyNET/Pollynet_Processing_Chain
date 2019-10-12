4280493549b0b1ae9411128e394c610d1ea68513
ZP.Yin <ZP.Yin@whu.edu.cn>
2019-10-12 13:03:43 +0200

    Bugfixes: enable load un-calibrated results
    Former-commit-id: 3c07ec7681021f8fa83c40dcd72d55c5492fd0e8
    Former-commit-id: 22bd3197d2fdca2da28933b9ffc7e5863966289f
    Former-commit-id: 5a9b20e2d410c06d9adeae45138f56828140b198

--- lib/pollyxt_tropos_func_lib/pollyxt_tropos_read_wvconst.m
+++ lib/pollyxt_tropos_func_lib/pollyxt_tropos_read_wvconst.m
@@ -1,4 +1,4 @@
-function [datetime, wvconst, wvconstStd] = pollyxt_tropos_read_wvconst(wvconstFile)
+function [datetime, wvCaliFlag, wvconst, wvconstStd] = pollyxt_tropos_read_wvconst(wvconstFile)
 %pollyxt_tropos_read_wvconst read the depolarization calibration results from the file.
 %   Example:
 %       [datetime, wvconst, wvconstStd] = pollyxt_tropos_read_wvconst(wvconstFile)
@@ -8,8 +8,10 @@ function [datetime, wvconst, wvconstStd] = pollyxt_tropos_read_wvconst(wvconstFi
 %   Outputs:
 %       datetime: array
 %           calibration time for each calibration period. 
+%       wvCaliFlag: array
+%           water vapor calibration status. (0: no calibration; 1: with calibration)
 %       wvconst: array
-%           water vapor calibration constant (the same to V*)
+%           water vapor calibration constant. (the same to V*)
 %       wvconstStd: array
 %           standard deviation of water vapor calibration constant. 
 %   History:
@@ -18,6 +20,7 @@ function [datetime, wvconst, wvconstStd] = pollyxt_tropos_read_wvconst(wvconstFi
 %       zhenping@tropos.de
 
 datetime = [];
+wvCaliFlag = [];
 wvconst = [];
 wvconstStd = [];
 
@@ -30,8 +33,9 @@ data = textscan(fid, '%s %d %s %s %s %f %f', 'delimiter', ',', 'Headerlines', 1)
 fclose(fid);
 
 for iRow = 1:length(data{1})
-    if data{2}(iRow) == 1 && (~ strcmp(data{3}{iRow}, '-999'))
+    if (~ strcmp(data{3}{iRow}, '-999'))
         datetime = [datetime, datenum(data{3}{iRow}, 'yyyymmdd HH:MM')];
+        wvCaliFlag = [wvCaliFlag, data{2}(iRow)];
         wvconst = [wvconst, data{6}(iRow)];
         wvconstStd = [wvconstStd, data{7}(iRow)];
     end
