46b98e8813abf5a97c9dee90cb587ee9825f6b72
ZP.Yin <ZP.Yin@whu.edu.cn>
2019-10-13 12:20:11 +0200

    format the python code: pollyxt_dwd
    Former-commit-id: e37b63444ed4d1ff8987320c337ce2f1a8bccb82
    Former-commit-id: afeb5be6de17a8c321f62bb1a28fec6e11c45179
    Former-commit-id: 11c0b2dacb73bf6cf15ef40c0e1c1baebd5f54f2

--- lib/pollyxt_dwd_func_lib/pollyxt_dwd_display_WV.py
+++ lib/pollyxt_dwd_func_lib/pollyxt_dwd_display_WV.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from matplotlib.colors import ListedColormap
 from matplotlib.dates import DateFormatter, DayLocator, HourLocator, \
-                             MinuteLocator, date2num
+    MinuteLocator, date2num
 matplotlib.use('Agg')
 
 
@@ -74,17 +74,17 @@ def rmext(filename):
 
 
 def pollyxt_dwd_display_WV(tmpFile, saveFolder):
-    '''
+    """
     Description
     -----------
     Display the housekeeping data from laserlogbook file.
 
     Parameters
     ----------
-    - tmpFile: the .mat file which stores the housekeeping data.
+    tmpFile: str
+    the .mat file which stores the housekeeping data.
 
-    Return
-    ------ 
+    saveFolder: str
 
     Usage
     -----
@@ -93,16 +93,12 @@ def pollyxt_dwd_display_WV(tmpFile, saveFolder):
     History
     -------
     2019-01-10. First edition by Zhenping
-
-    Copyright
-    ---------
-    Ground-based Remote Sensing (TROPOS)
-    '''
+    """
 
     if not os.path.exists(tmpFile):
         print('{filename} does not exists.'.format(filename=tmpFile))
         return
-    
+
     # read matlab .mat data
     try:
         mat = spio.loadmat(tmpFile, struct_as_record=True)
@@ -145,7 +141,12 @@ def pollyxt_dwd_display_WV(tmpFile, saveFolder):
     # display WVMR
     fig = plt.figure(figsize=[10, 5])
     ax = fig.add_axes([0.1, 0.15, 0.8, 0.75])
-    pcmesh = ax.pcolormesh(Time, Height, WVMR, vmin=WVMRColorRange[0], vmax=WVMRColorRange[1], cmap=cmap)
+    pcmesh = ax.pcolormesh(
+        Time, Height, WVMR,
+        vmin=WVMRColorRange[0],
+        vmax=WVMRColorRange[1],
+        cmap=cmap
+        )
     ax.set_xlabel('UTC', fontsize=15)
     ax.set_ylabel('Height (m)', fontsize=15)
 
@@ -154,21 +155,37 @@ def pollyxt_dwd_display_WV(tmpFile, saveFolder):
     ax.yaxis.set_minor_locator(MultipleLocator(500))
     ax.set_xticks(xtick.tolist())
     ax.set_xticklabels(celltolist(xticklabel))
-    ax.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
-    ax.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+    ax.tick_params(axis='both', which='major', labelsize=15,
+                   right=True, top=True, width=2, length=5)
+    ax.tick_params(axis='both', which='minor', width=1.5,
+                   length=3.5, right=True, top=True)
 
-    ax.set_title('Water vapor mixing ratio from {instrument} at {location}'.format(instrument=pollyVersion, location=location), fontsize=15)
+    ax.set_title(
+        'Water vapor mixing ratio from {instrument} at {location}'.format(
+            instrument=pollyVersion, location=location
+            ),
+        fontsize=15
+        )
 
     cb_ax = fig.add_axes([0.92, 0.20, 0.02, 0.65])
-    cbar = fig.colorbar(pcmesh, cax=cb_ax, ticks=np.linspace(WVMRColorRange[0], WVMRColorRange[1], 5), orientation='vertical')
+    cbar = fig.colorbar(pcmesh, cax=cb_ax, ticks=np.linspace(
+        WVMRColorRange[0], WVMRColorRange[1], 5), orientation='vertical')
     cbar.ax.tick_params(direction='in', labelsize=15, pad=10)
     cbar.ax.set_title('[$g*kg^{-1}$]', fontsize=10)
 
-    fig.text(0.05, 0.02, '{time}\nMeteor Data: {meteorSource}'.format(time=datenum_to_datetime(time[0]).strftime("%Y-%m-%d"), meteorSource=meteorSource), fontsize=12)
-    fig.text(0.8, 0.02, 'Version: {version}\nCalibration: {status}'.format(version=version, status=flagCalibrated), fontsize=12)
+    fig.text(
+        0.05, 0.02,
+        '{time}\nMeteor Data: {meteorSource}'.format(
+            time=datenum_to_datetime(time[0]).strftime("%Y-%m-%d"),
+            meteorSource=meteorSource
+            ),
+        fontsize=12
+        )
+    fig.text(0.8, 0.02, 'Version: {version}\nCalibration: {status}'.format(
+        version=version, status=flagCalibrated), fontsize=12)
 
-    
-    fig.savefig(os.path.join(saveFolder, '{dataFilename}_WVMR.png'.format(dataFilename=rmext(dataFilename))), dpi=figDPI)
+    fig.savefig(os.path.join(saveFolder, '{dataFilename}_WVMR.png'.format(
+        dataFilename=rmext(dataFilename))), dpi=figDPI)
     plt.close()
 
     # display RH
@@ -183,25 +200,42 @@ def pollyxt_dwd_display_WV(tmpFile, saveFolder):
     ax.yaxis.set_minor_locator(MultipleLocator(500))
     ax.set_xticks(xtick.tolist())
     ax.set_xticklabels(celltolist(xticklabel))
-    ax.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
-    ax.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+    ax.tick_params(axis='both', which='major', labelsize=15,
+                   right=True, top=True, width=2, length=5)
+    ax.tick_params(axis='both', which='minor', width=1.5,
+                   length=3.5, right=True, top=True)
 
-    ax.set_title('Relative humidity from {instrument} at {location}'.format(instrument=pollyVersion, location=location), fontsize=15)
+    ax.set_title('Relative humidity from {instrument} at {location}'.format(
+        instrument=pollyVersion, location=location), fontsize=15)
 
     cb_ax = fig.add_axes([0.92, 0.20, 0.02, 0.65])
-    cbar = fig.colorbar(pcmesh, cax=cb_ax, ticks=np.arange(0, 100.1, 20), orientation='vertical')
+    cbar = fig.colorbar(pcmesh, cax=cb_ax, ticks=np.arange(
+        0, 100.1, 20), orientation='vertical')
     cbar.ax.tick_params(direction='in', labelsize=15, pad=10)
     cbar.ax.set_title('[$\%$]', fontsize=10)
 
-    fig.text(0.05, 0.02, '{time}\nMeteor Data: {meteorSource}'.format(time=datenum_to_datetime(time[0]).strftime("%Y-%m-%d"), meteorSource=meteorSource), fontsize=12)
-    fig.text(0.8, 0.02, 'Version: {version}\nCalibration: {status}'.format(version=version, status=flagCalibrated), fontsize=12)
+    fig.text(
+        0.05, 0.02,
+        '{time}\nMeteor Data: {meteorSource}'.format(
+            time=datenum_to_datetime(time[0]).strftime("%Y-%m-%d"),
+            meteorSource=meteorSource
+            ),
+        fontsize=12
+        )
+    fig.text(0.8, 0.02, 'Version: {version}\nCalibration: {status}'.format(
+        version=version, status=flagCalibrated), fontsize=12)
 
-    
-    fig.savefig(os.path.join(saveFolder, '{dataFilename}_RH.png'.format(dataFilename=rmext(dataFilename))), dpi=figDPI)
+    fig.savefig(os.path.join(saveFolder, '{dataFilename}_RH.png'.format(
+        dataFilename=rmext(dataFilename))), dpi=figDPI)
     plt.close()
 
+
 def main():
-    pollyxt_dwd_display_WV('C:\\Users\\zhenping\\Desktop\\Picasso\\tmp\\tmp.mat', 'C:\\Users\\zhenping\\Desktop')
+    pollyxt_dwd_display_WV(
+        'C:\\Users\\zhenping\\Desktop\\Picasso\\tmp\\tmp.mat',
+        'C:\\Users\\zhenping\\Desktop'
+        )
+
 
 if __name__ == '__main__':
     # main()
--- lib/pollyxt_dwd_func_lib/pollyxt_dwd_display_lidarconst.py
+++ lib/pollyxt_dwd_func_lib/pollyxt_dwd_display_lidarconst.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from matplotlib.colors import ListedColormap
 from matplotlib.dates import DateFormatter, DayLocator, HourLocator, \
-                             MinuteLocator, date2num
+    MinuteLocator, date2num
 matplotlib.use('Agg')
 
 
@@ -81,10 +81,10 @@ def pollyxt_dwd_display_lidarconst(tmpFile, saveFolder):
 
     Parameters
     ----------
-    - tmpFile: the .mat file which stores the housekeeping data.
+    tmpFile: str
+    the .mat file which stores the housekeeping data.
 
-    Return
-    ------ 
+    saveFolder: str
 
     Usage
     -----
@@ -93,16 +93,12 @@ def pollyxt_dwd_display_lidarconst(tmpFile, saveFolder):
     History
     -------
     2019-01-10. First edition by Zhenping
-
-    Copyright
-    ---------
-    Ground-based Remote Sensing (TROPOS)
     '''
 
     if not os.path.exists(tmpFile):
         print('{filename} does not exists.'.format(filename=tmpFile))
         return
-    
+
     # read matlab .mat data
     try:
         mat = spio.loadmat(tmpFile, struct_as_record=True)
@@ -139,12 +135,22 @@ def pollyxt_dwd_display_lidarconst(tmpFile, saveFolder):
     # display lidar constants at 355mn
     fig = plt.figure(figsize=[9, 5])
     ax = fig.add_axes([0.1, 0.15, 0.8, 0.75])
-    p1, = ax.plot(thisTime, LC355_klett, color='#008040', linestyle='--', marker='^', markersize=10, mfc='#008040', mec='#000000', label='Klett Method')
-    p2, = ax.plot(thisTime, LC355_raman, color='#400080', linestyle='--', marker='o', markersize=10, mfc='#400080', mec='#000000', label='Raman Method')
-    p3, = ax.plot(thisTime, LC355_aeronet, color='#804000', linestyle='--', marker='*', markersize=10, mfc='#800040', mec='#000000', label='Constrained-AOD Method')
+    p1, = ax.plot(
+        thisTime, LC355_klett,
+        color='#008040', linestyle='--', marker='^',
+        markersize=10, mfc='#008040', mec='#000000', label='Klett Method')
+    p2, = ax.plot(
+        thisTime, LC355_raman,
+        color='#400080', linestyle='--', marker='o',
+        markersize=10, mfc='#400080', mec='#000000', label='Raman Method')
+    p3, = ax.plot(
+        thisTime, LC355_aeronet,
+        color='#804000', linestyle='--', marker='*',
+        markersize=10, mfc='#800040', mec='#000000',
+        label='Constrained-AOD Method')
     ax.set_xlabel('UTC', fontsize=15)
     ax.set_ylabel('C', fontsize=15)
-    l = ax.legend(handles=[p1, p2, p3], loc='upper right', fontsize=12)
+    ax.legend(handles=[p1, p2, p3], loc='upper right', fontsize=12)
 
     ax.set_ylim(yLim355.tolist())
     ax.set_xticks(xtick.tolist())
@@ -152,26 +158,44 @@ def pollyxt_dwd_display_lidarconst(tmpFile, saveFolder):
     ax.set_xticklabels(celltolist(xticklabel))
     ax.grid(True)
 
-    ax.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
-    ax.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+    ax.tick_params(axis='both', which='major', labelsize=15,
+                   right=True, top=True, width=2, length=5)
+    ax.tick_params(axis='both', which='minor', width=1.5,
+                   length=3.5, right=True, top=True)
 
-    ax.set_title('Lidar constants {wave}nm Far-Range for {instrument} at {location}'.format(wave=355, instrument=pollyVersion, location=location), fontsize=15)
+    ax.set_title(
+        'Lidar constants ' +
+        '{wave}nm Far-Range for {instrument} at {location}'.format(
+            wave=355, instrument=pollyVersion, location=location), fontsize=15)
 
-    fig.text(0.05, 0.02, datenum_to_datetime(time[0]).strftime("%Y-%m-%d"), fontsize=12)
-    fig.text(0.8, 0.02, 'Version: {version}'.format(version=version), fontsize=12)
+    fig.text(0.05, 0.02, datenum_to_datetime(
+        time[0]).strftime("%Y-%m-%d"), fontsize=12)
+    fig.text(0.8, 0.02, 'Version: {version}'.format(
+        version=version), fontsize=12)
 
-    fig.savefig(os.path.join(saveFolder, '{dataFilename}_LC_355.png'.format(dataFilename=rmext(dataFilename))), dpi=figDPI)
+    fig.savefig(os.path.join(saveFolder, '{dataFilename}_LC_355.png'.format(
+        dataFilename=rmext(dataFilename))), dpi=figDPI)
     plt.close()
 
     # display lidar constants at 532mn
     fig = plt.figure(figsize=[9, 5])
     ax = fig.add_axes([0.1, 0.15, 0.8, 0.75])
-    p1, = ax.plot(thisTime, LC532_klett, color='#008040', linestyle='--', marker='^', markersize=10, mfc='#008040', mec='#000000', label='Klett Method')
-    p2, = ax.plot(thisTime, LC532_raman, color='#400080', linestyle='--', marker='o', markersize=10, mfc='#400080', mec='#000000', label='Raman Method')
-    p3, = ax.plot(thisTime, LC532_aeronet, color='#804000', linestyle='--', marker='*', markersize=10, mfc='#800040', mec='#000000', label='Constrained-AOD Method')
+    p1, = ax.plot(
+        thisTime, LC532_klett,
+        color='#008040', linestyle='--', marker='^',
+        markersize=10, mfc='#008040', mec='#000000', label='Klett Method')
+    p2, = ax.plot(
+        thisTime, LC532_raman,
+        color='#400080', linestyle='--', marker='o',
+        markersize=10, mfc='#400080', mec='#000000', label='Raman Method')
+    p3, = ax.plot(
+        thisTime, LC532_aeronet,
+        color='#804000', linestyle='--', marker='*',
+        markersize=10, mfc='#800040', mec='#000000',
+        label='Constrained-AOD Method')
     ax.set_xlabel('UTC', fontsize=15)
     ax.set_ylabel('C', fontsize=15)
-    l = ax.legend(handles=[p1, p2, p3], loc='upper right', fontsize=12)
+    ax.legend(handles=[p1, p2, p3], loc='upper right', fontsize=12)
 
     ax.set_ylim(yLim532.tolist())
     ax.set_xticks(xtick.tolist())
@@ -179,26 +203,44 @@ def pollyxt_dwd_display_lidarconst(tmpFile, saveFolder):
     ax.set_xticklabels(celltolist(xticklabel))
     ax.grid(True)
 
-    ax.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
-    ax.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+    ax.tick_params(axis='both', which='major', labelsize=15,
+                   right=True, top=True, width=2, length=5)
+    ax.tick_params(axis='both', which='minor', width=1.5,
+                   length=3.5, right=True, top=True)
 
-    ax.set_title('Lidar constants {wave}nm Far-Range for {instrument} at {location}'.format(wave=532, instrument=pollyVersion, location=location), fontsize=15)
+    ax.set_title(
+        'Lidar constants ' +
+        '{wave}nm Far-Range for {instrument} at {location}'.format(
+            wave=532, instrument=pollyVersion, location=location), fontsize=15)
 
-    fig.text(0.05, 0.02, datenum_to_datetime(time[0]).strftime("%Y-%m-%d"), fontsize=12)
-    fig.text(0.8, 0.02, 'Version: {version}'.format(version=version), fontsize=12)
+    fig.text(0.05, 0.02, datenum_to_datetime(
+        time[0]).strftime("%Y-%m-%d"), fontsize=12)
+    fig.text(0.8, 0.02, 'Version: {version}'.format(
+        version=version), fontsize=12)
 
-    fig.savefig(os.path.join(saveFolder, '{dataFilename}_LC_532.png'.format(dataFilename=rmext(dataFilename))), dpi=figDPI)
+    fig.savefig(os.path.join(saveFolder, '{dataFilename}_LC_532.png'.format(
+        dataFilename=rmext(dataFilename))), dpi=figDPI)
     plt.close()
-    
+
     # display lidar constants at 1064mn
     fig = plt.figure(figsize=[9, 5])
     ax = fig.add_axes([0.1, 0.15, 0.8, 0.75])
-    p1, = ax.plot(thisTime, LC1064_klett, color='#008040', linestyle='--', marker='^', markersize=10, mfc='#008040', mec='#000000', label='Klett Method')
-    p2, = ax.plot(thisTime, LC1064_raman, color='#400080', linestyle='--', marker='o', markersize=10, mfc='#400080', mec='#000000', label='Raman Method')
-    p3, = ax.plot(thisTime, LC1064_aeronet, color='#804000', linestyle='--', marker='*', markersize=10, mfc='#800040', mec='#000000', label='Constrained-AOD Method')
+    p1, = ax.plot(
+        thisTime, LC1064_klett,
+        color='#008040', linestyle='--', marker='^',
+        markersize=10, mfc='#008040', mec='#000000', label='Klett Method')
+    p2, = ax.plot(
+        thisTime, LC1064_raman,
+        color='#400080', linestyle='--', marker='o',
+        markersize=10, mfc='#400080', mec='#000000', label='Raman Method')
+    p3, = ax.plot(
+        thisTime, LC1064_aeronet,
+        color='#804000', linestyle='--', marker='*',
+        markersize=10, mfc='#800040', mec='#000000',
+        label='Constrained-AOD Method')
     ax.set_xlabel('UTC', fontsize=15)
     ax.set_ylabel('C', fontsize=15)
-    l = ax.legend(handles=[p1, p2, p3], loc='upper right', fontsize=12)
+    ax.legend(handles=[p1, p2, p3], loc='upper right', fontsize=12)
 
     ax.set_ylim(yLim1064.tolist())
     ax.set_xticks(xtick.tolist())
@@ -206,19 +248,32 @@ def pollyxt_dwd_display_lidarconst(tmpFile, saveFolder):
     ax.set_xticklabels(celltolist(xticklabel))
     ax.grid(True)
 
-    ax.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
-    ax.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+    ax.tick_params(axis='both', which='major', labelsize=15,
+                   right=True, top=True, width=2, length=5)
+    ax.tick_params(axis='both', which='minor', width=1.5,
+                   length=3.5, right=True, top=True)
 
-    ax.set_title('Lidar constants {wave}nm Far-Range for {instrument} at {location}'.format(wave=1064, instrument=pollyVersion, location=location), fontsize=15)
+    ax.set_title(
+        'Lidar constants ' +
+        '{wave}nm Far-Range for {instrument} at {location}'.format(
+            wave=1064,
+            instrument=pollyVersion, location=location), fontsize=15)
 
-    fig.text(0.05, 0.02, datenum_to_datetime(time[0]).strftime("%Y-%m-%d"), fontsize=12)
-    fig.text(0.8, 0.02, 'Version: {version}'.format(version=version), fontsize=12)
+    fig.text(0.05, 0.02, datenum_to_datetime(
+        time[0]).strftime("%Y-%m-%d"), fontsize=12)
+    fig.text(0.8, 0.02, 'Version: {version}'.format(
+        version=version), fontsize=12)
 
-    fig.savefig(os.path.join(saveFolder, '{dataFilename}_LC_1064.png'.format(dataFilename=rmext(dataFilename))), dpi=figDPI)
+    fig.savefig(os.path.join(saveFolder, '{dataFilename}_LC_1064.png'.format(
+        dataFilename=rmext(dataFilename))), dpi=figDPI)
     plt.close()
 
+
 def main():
-    pollyxt_dwd_display_lidarconst('C:\\Users\\zhenping\\Desktop\\Picasso\\tmp\\tmp.mat', 'C:\\Users\\zhenping\\Desktop')
+    pollyxt_dwd_display_lidarconst(
+        'C:\\Users\\zhenping\\Desktop\\Picasso\\tmp\\tmp.mat',
+        'C:\\Users\\zhenping\\Desktop')
+
 
 if __name__ == '__main__':
     # main()
