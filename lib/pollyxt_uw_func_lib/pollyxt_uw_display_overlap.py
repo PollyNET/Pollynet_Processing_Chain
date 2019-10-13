4f3c16942bc7d343e1b7eae249097c0add8e5842
ZP.Yin <ZP.Yin@whu.edu.cn>
2019-10-13 10:02:46 +0200

    Format the python code
    Format the python code with the PEP8 standard. But this work was not done for 'polly_1v2', 'pollyxt_ift', 'pollyxt_cge' and 'pollyxt_dwd', which have different channel settings, thus they had different python code.

--- lib/pollyxt_uw_func_lib/pollyxt_uw_display_overlap.py
+++ lib/pollyxt_uw_func_lib/pollyxt_uw_display_overlap.py
@@ -1,23 +1,49 @@
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from matplotlib.dates import DateFormatter, DayLocator, HourLocator, MinuteLocator, date2num
+from matplotlib.dates import DateFormatter, DayLocator, HourLocator, \
+    MinuteLocator, date2num
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
-import os, sys
+import os
+import sys
 import scipy.io as spio
 import numpy as np
 from datetime import datetime, timedelta
+import re
+matplotlib.use('Agg')
+
 
 def parse_polly_filename(pollyFile):
-    import re
+    """
+    parse the datetime from the polly data filename.
+    """
 
     psFmt = r"^(\d{4})_(\d{2})_(\d{2}).*_(\d{2})_(\d{2})_(\d{2})*.*"
     items = re.search(psFmt, pollyFile)
 
-    return datetime(int(items[1]), int(items[2]), int(items[3]), int(items[4]), int(items[5]), int(items[6]))
+    dt = datetime(
+        int(items[1]),
+        int(items[2]),
+        int(items[3]),
+        int(items[4]),
+        int(items[5]),
+        int(items[6])
+    )
+
+    return dt
+
 
 def celltolist(xtickstr):
+    """
+    convert list of list to list of string.
+
+    Examples
+    --------
+
+    [['2010-10-11'], [], ['2011-10-12]] =>
+    ['2010-10-11], '', '2011-10-12']
+    """
+
     tmp = []
     for iElement in range(0, len(xtickstr)):
         if not len(xtickstr[iElement][0]):
@@ -27,58 +53,74 @@ def celltolist(xtickstr):
 
     return tmp
 
+
 def datenum_to_datetime(datenum):
     """
     Convert Matlab datenum into Python datetime.
-    :param datenum: Date in datenum format
-    :return:        Datetime object corresponding to datenum.
+
+    Parameters
+    ----------
+    Date: float
+
+    Returns
+    -------
+    dtObj: datetime object
+
     """
     days = datenum % 1
     hours = days % 1 * 24
     minutes = hours % 1 * 60
     seconds = minutes % 1 * 60
-    return datetime.fromordinal(int(datenum)) \
-           + timedelta(days=int(days)) \
-           + timedelta(hours=int(hours)) \
-           + timedelta(minutes=int(minutes)) \
-           + timedelta(seconds=round(seconds)) \
-            - timedelta(days=366)
+
+    dtObj = datetime.fromordinal(int(datenum)) + \
+        timedelta(days=int(days)) + \
+        timedelta(hours=int(hours)) + \
+        timedelta(minutes=int(minutes)) + \
+        timedelta(seconds=round(seconds)) - timedelta(days=366)
+
+    return dtObj
+
 
 def rmext(filename):
+    """
+    remove the file extension.
+
+    Parameters
+    ----------
+    filename: str
+    """
+
     file, _ = os.path.splitext(filename)
     return file
 
+
 def pollyxt_uw_display_overlap(tmpFile, saveFolder):
-    '''
+    """
     Description
     -----------
     Display the housekeeping data from laserlogbook file.
 
     Parameters
     ----------
-    - tmpFile: the .mat file which stores the housekeeping data.
+    tmpFile: str
+    the .mat file which stores the housekeeping data.
 
-    Return
-    ------ 
+    saveFolder: str
 
     Usage
     -----
-    pollyxt_uw_display_overlap(tmpFile)
+    pollyxt_uw_display_overlap(tmpFile, saveFolder)
 
     History
     -------
     2019-01-10. First edition by Zhenping
-
-    Copyright
-    ---------
-    Ground-based Remote Sensing (TROPOS)
-    '''
+    """
 
     if not os.path.exists(tmpFile):
         print('{filename} does not exists.'.format(filename=tmpFile))
         return
-    
-    # read data
+
+    # read matlab .mat data
     try:
         mat = spio.loadmat(tmpFile, struct_as_record=True)
         figDPI = mat['figDPI'][0][0]
@@ -111,28 +153,46 @@ def pollyxt_uw_display_overlap(tmpFile, saveFolder):
     matplotlib.rcParams['font.sans-serif'] = fontname
     matplotlib.rcParams['font.family'] = "sans-serif"
 
-
     # display
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 8), sharey=True, gridspec_kw={'width_ratios': [1.2, 1]})
+    fig, (ax1, ax2) = plt.subplots(
+        1, 2, figsize=(8, 8),
+        sharey=True,
+        gridspec_kw={'width_ratios': [1.2, 1]}
+        )
     fig.subplots_adjust(wspace=0)
 
     # display signal
-    p1, = ax1.plot(overlap355, height, color='#1544E9', linestyle='-', label=r'overlap 355 FR')
-    p2, = ax1.plot(overlap532, height, color='#58B13F', linestyle='-', label=r'overlap 532 FR')
-    p3, = ax1.plot(overlap355Defaults, height, color='#1544E9', linestyle='--', label=r'default overlap 355 FR')
-    p4, = ax1.plot(overlap532Defaults, height, color='#58B13F', linestyle='--', label=r'default overlap 532 FR')
+    p1, = ax1.plot(overlap355, height, color='#1544E9',
+                   linestyle='-', label=r'overlap 355 FR')
+    p2, = ax1.plot(overlap532, height, color='#58B13F',
+                   linestyle='-', label=r'overlap 532 FR')
+    p3, = ax1.plot(overlap355Defaults, height, color='#1544E9',
+                   linestyle='--', label=r'default overlap 355 FR')
+    p4, = ax1.plot(overlap532Defaults, height, color='#58B13F',
+                   linestyle='--', label=r'default overlap 532 FR')
     ax1.set_ylim([0, 3000])
     ax1.set_xlim([-0.05, 1.1])
     ax1.set_ylabel('Height (m)', fontsize=15)
     ax1.set_xlabel('Overlap', fontsize=15)
-    ax1.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
-    ax1.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+    ax1.tick_params(axis='both', which='major', labelsize=15,
+                    right=True, top=True, width=2, length=5)
+    ax1.tick_params(axis='both', which='minor', width=1.5,
+                    length=3.5, right=True, top=True)
     ax1.grid(True)
     ax1.yaxis.set_major_locator(MultipleLocator(500))
     ax1.yaxis.set_minor_locator(MultipleLocator(100))
     start = parse_polly_filename(dataFilename)
-    fig.text(0.5, 0.98, 'Overlap for {instrument} at {location}, {time}'.format(instrument=pollyVersion, location=location, time=start.strftime('%Y%m%d %H:%M')), horizontalalignment='center', fontsize=15)
-    l = ax1.legend(handles=[p1, p2, p3, p4], loc='upper left', fontsize=15)
+    fig.text(
+        0.5, 0.98,
+        'Overlap for {instrument} at {location}, {time}'.format(
+            instrument=pollyVersion,
+            location=location,
+            time=start.strftime('%Y%m%d %H:%M')
+            ),
+        horizontalalignment='center',
+        fontsize=15
+        )
+    ax1.legend(handles=[p1, p2, p3, p4], loc='upper left', fontsize=15)
 
     sig355FR = np.ma.masked_where(sig355FR <= 0, sig355FR)
     sig355NR = np.ma.masked_where(sig355NR <= 0, sig355NR)
@@ -140,35 +200,73 @@ def pollyxt_uw_display_overlap(tmpFile, saveFolder):
     sig532FR = np.ma.masked_where(sig532FR <= 0, sig532FR)
     sig532NR = np.ma.masked_where(sig532NR <= 0, sig532NR)
     sig532Gl = np.ma.masked_where(sig532Gl <= 0, sig532Gl)
-    p1, = ax2.semilogx(sig355FR, height, color='#1544E9', linestyle='-.', label=r'FR 355')
-    p2, = ax2.semilogx(sig355NR, height, color='#1544E9', linestyle=':', label=r'NR 355')
-    p3, = ax2.semilogx(sig355Gl, height, color='#1544E9', linestyle='-', label=r'FR Glued 355')
-    p4, = ax2.semilogx(sig532FR, height, color='#58B13F', linestyle='-.', label=r'FR 532')
-    p5, = ax2.semilogx(sig532NR, height, color='#58B13F', linestyle=':', label=r'NR 532')
-    p6, = ax2.semilogx(sig532Gl, height, color='#58B13F', linestyle='-', label=r'FR Glued 532')
+    p1, = ax2.semilogx(sig355FR, height, color='#1544E9',
+                       linestyle='-.', label=r'FR 355')
+    p2, = ax2.semilogx(sig355NR, height, color='#1544E9',
+                       linestyle=':', label=r'NR 355')
+    p3, = ax2.semilogx(sig355Gl, height, color='#1544E9',
+                       linestyle='-', label=r'FR Glued 355')
+    p4, = ax2.semilogx(sig532FR, height, color='#58B13F',
+                       linestyle='-.', label=r'FR 532')
+    p5, = ax2.semilogx(sig532NR, height, color='#58B13F',
+                       linestyle=':', label=r'NR 532')
+    p6, = ax2.semilogx(sig532Gl, height, color='#58B13F',
+                       linestyle='-', label=r'FR Glued 532')
 
     if normRange355.size != 0:
-        ax2.plot([1e-10, 1e10], [height[normRange355[0] - 1], height[normRange355[0] - 1]], linestyle='--', color='#1544E9')
-        ax2.plot([1e-10, 1e10], [height[normRange355[-1] - 1], height[normRange355[-1] - 1]], linestyle='--', color='#1544E9')
+        ax2.plot(
+            [1e-10, 1e10],
+            [height[normRange355[0] - 1], height[normRange355[0] - 1]],
+            linestyle='--',
+            color='#1544E9'
+            )
+        ax2.plot(
+            [1e-10, 1e10],
+            [height[normRange355[-1] - 1], height[normRange355[-1] - 1]],
+            linestyle='--',
+            color='#1544E9'
+            )
     if normRange532.size != 0:
-        ax2.plot([1e-10, 1e10], [height[normRange532[0] - 1], height[normRange532[0] - 1]], linestyle='--', color='#58B13F')
-        ax2.plot([1e-10, 1e10], [height[normRange532[-1] - 1], height[normRange532[-1] - 1]], linestyle='--', color='#58B13F')
-    
+        ax2.plot(
+            [1e-10, 1e10],
+            [height[normRange532[0] - 1], height[normRange532[0] - 1]],
+            linestyle='--',
+            color='#58B13F'
+            )
+        ax2.plot(
+            [1e-10, 1e10],
+            [height[normRange532[-1] - 1], height[normRange532[-1] - 1]],
+            linestyle='--',
+            color='#58B13F'
+            )
+
     ax2.set_xlim([1e-2, 1e3])
     ax2.set_xlabel('Signal [MHz]', fontsize=15)
-    ax2.tick_params(axis='both', which='major', labelsize=15, right=True, top=True, width=2, length=5)
-    ax2.tick_params(axis='both', which='minor', width=1.5, length=3.5, right=True, top=True)
+    ax2.tick_params(axis='both', which='major', labelsize=15,
+                    right=True, top=True, width=2, length=5)
+    ax2.tick_params(axis='both', which='minor', width=1.5,
+                    length=3.5, right=True, top=True)
     ax2.grid(True)
-    l = ax2.legend(handles=[p1, p2, p3, p4, p5, p6], loc='upper right', fontsize=15)
+    ax2.legend(
+        handles=[p1, p2, p3, p4, p5, p6],
+        loc='upper right', fontsize=15
+        )
 
-    fig.text(0.87, 0.02, 'Version {version}'.format(version=version), fontsize=10)
+    fig.text(0.87, 0.02, 'Version {version}'.format(
+        version=version), fontsize=10)
     # plt.tight_layout()
-    fig.savefig(os.path.join(saveFolder, '{dataFilename}_overlap.png'.format(dataFilename=rmext(dataFilename))), dpi=figDPI)
- 
+    fig.savefig(os.path.join(saveFolder, '{dataFilename}_overlap.png'.format(
+        dataFilename=rmext(dataFilename))), dpi=figDPI)
+
     plt.close()
 
+
 def main():
-    pollyxt_uw_display_overlap('C:\\Users\\zhenping\\Desktop\\Picasso\\tmp\\tmp.mat', 'C:\\Users\\zhenping\\Desktop')
+    pollyxt_uw_display_overlap(
+        'C:\\Users\\zhenping\\Desktop\\Picasso\\tmp\\tmp.mat',
+        'C:\\Users\\zhenping\\Desktop'
+        )
+
 
 if __name__ == '__main__':
     # main()
