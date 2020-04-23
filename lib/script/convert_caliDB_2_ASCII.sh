f82a4bed3e0f14d73212335407aeb084ff21ca5b
ZP.Yin <ZP.Yin@whu.edu.cn>
2020-04-23 23:41:59 +0800

    improvement: script to convert DB to ASCII
    add shell script to convert all DB file from Picasso to ASCII. This can be deployed for cronjob in
    the rsd server.
    
    
    Former-commit-id: 24d2920edb52f17ec3db9058c2539bcded00de39
    Former-commit-id: 10a64ec8b803301fcd387758068c12d237baf859
    Former-commit-id: b8c5f93452a2169fbf3d817911669a8ebaecefef

---
+++ lib/script/convert_caliDB_2_ASCII.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This script is used to convert the SQLite DB of lidar calibration data to
+# ASCII files.
+
+cwd="$( cd "$(dirname "$0")"; pwd -P )"
+PATH=${PATH}:$cwd
+
+RESULTS_FOLDER="$1"
+
+matlab -nodisplay -nodesktop -nosplash <<ENDMATLAB
+
+resultsFolder = '$RESULTS_FOLDER';
+projectFolder = fileparts(fileparts('$cwd'));
+
+addpath(genpath(fullfile(projectFolder, 'lib')));
+
+subFolders = listdir(resultsFolder);
+
+for iFolder = 1:length(subFolders)
+
+    dbFiles = listfile(subFolders{iFolder}, '\w*.db');
+
+    for iDBFile = 1:length(dbFiles)
+        dbBasename = basename(dbFiles{iDBFile});
+        extract_cali_results(dbFiles{iDBFile}, subFolders{iFolder}, 'prefix', dbBasename(1:(end-3)));
+    end
+
+end
+
+ENDMATLAB
