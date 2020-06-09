13ddde1e908c8234b32d5c7095c8d49f929b6022
Holger Baars <baars@TROPOS.DE>
2020-06-09 14:34:57 +0200

    added option to adjust volume depol z scale

--- lib/polly_general_func_lib/pollyxt_display_rcs.m
+++ lib/polly_general_func_lib/pollyxt_display_rcs.m
@@ -30,6 +30,8 @@ RCS532FRColorRange = config.zLim_FR_RCS_532;
 RCS1064FRColorRange = config.zLim_FR_RCS_1064;
 RCS355NRColorRange = config.zLim_NR_RCS_355;
 RCS532NRColorRange = config.zLim_NR_RCS_355;
+Voldepol355ColorRange = config.zLim_VolDepol_355;
+Voldepol532ColorRange = config.zLim_VolDepol_532;
 imgFormat = config.imgFormat;
 
 if strcmpi(processInfo.visualizationMode, 'matlab')
@@ -341,7 +343,7 @@ elseif strcmpi(processInfo.visualizationMode, 'python')
 
     %% display rcs 
     tmpFile = fullfile(tmpFolder, [basename(tempname), '.mat']);
-    save(tmpFile, 'figDPI', 'mTime', 'height', 'depCalMask', 'fogMask', 'yLim_FR_RCS', 'yLim_NR_RCS', 'yLim_FR_DR', 'RCS_FR_355', 'RCS_FR_532', 'RCS_FR_1064', 'RCS_NR_355', 'RCS_NR_532', 'volDepol_355', 'volDepol_532', 'processInfo', 'campaignInfo', 'taskInfo', 'xtick', 'xtickstr', 'RCS355FRColorRange', 'RCS532FRColorRange', 'RCS1064FRColorRange', 'RCS355NRColorRange', 'RCS532NRColorRange', 'imgFormat', '-v6');
+    save(tmpFile, 'figDPI', 'mTime', 'height', 'depCalMask', 'fogMask', 'yLim_FR_RCS', 'yLim_NR_RCS', 'yLim_FR_DR', 'RCS_FR_355', 'RCS_FR_532', 'RCS_FR_1064', 'RCS_NR_355', 'RCS_NR_532', 'volDepol_355', 'volDepol_532', 'processInfo', 'campaignInfo', 'taskInfo', 'xtick', 'xtickstr', 'RCS355FRColorRange', 'RCS532FRColorRange', 'RCS1064FRColorRange', 'RCS355NRColorRange', 'RCS532NRColorRange', 'Voldepol355ColorRange', 'Voldepol355ColorRange', 'imgFormat', '-v6');
     flag = system(sprintf('%s %s %s %s', fullfile(processInfo.pyBinDir, 'python'), fullfile(pyFolder, 'pollyxt_display_rcs.py'), tmpFile, saveFolder));
     if flag ~= 0
         warning('Error in executing %s', 'pollyxt_display_rcs.py');
