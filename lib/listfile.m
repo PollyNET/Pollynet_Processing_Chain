bcfe3490375081eaa0f0358345c61bc8035e2f82
zhenping <zp.yin@whu.edu.cn>
2020-07-18 14:42:09 +0800

    Bugfixes: update listfile
    Stop iterative searching when special folders (i.e.,'.' or '..') are
    found.

--- lib/listfile.m
+++ lib/listfile.m
@@ -1,9 +1,12 @@
-function [files, filesize] = listfile(path, exppat, depth)
+function [files, filesize] = listfile(inPath, exppat, depth)
 %LISTFILE list all the qualified files in path.
 %Usage:
+%   % Usecase 1: search matlab file at 'Desktop'
 %   a = listfile('~/Desktop', '*.m');
+%   % Usecase 2: search matlab file with folder depth of 2
+%   a = listfile('~/Desktop', '*.m', 2);
 %Inputs:
-%   path: char
+%   inPath: char
 %       the path for searching.
 %   exppat: [optional] 
 %       expression pattern for the searching.
@@ -17,15 +20,17 @@ function [files, filesize] = listfile(path, exppat, depth)
 %History:
 %   2018-07-25. First edition by Zhenping.
 %   2018-09-14. Add recursive searching depth.
-%   2019-09-03. Add the output of filesize
+%   2019-09-03. Add the output of filesize.
+%   2020-07-18. Stop interative search when searched the special folder of '..'
+%               or '.'
 %Contact:
 %   zhenping@tropos.de
 
 files = cell(0);
 filesize = cell(0);
 
-if ~ exist(path, 'dir')
-    warning('%s is not a valid directory.', path);
+if ~ exist(inPath, 'dir')
+    warning('%s is not a valid directory.', inPath);
     return
 end
 
@@ -33,7 +38,7 @@ if ~ exist('depth', 'var')
     depth = 1;
 end
 
-tmp = dir(path);
+tmp = dir(inPath);
 
 % no items in this directory
 if isempty(tmp)
@@ -46,20 +51,20 @@ for iItem = 1:length(tmp)
     if ~ tmp(iItem).isdir
         if exist('exppat', 'var')
             if regexp(tmp(iItem).name, exppat)
-                % if there is required items, return the results
+                % if there are required items, return the results
                 indx = indx + 1;
-                files{indx} = fullfile(path, tmp(iItem).name);
+                files{indx} = fullfile(inPath, tmp(iItem).name);
                 filesize{indx} = tmp(iItem).bytes;
             end
         else
             indx = indx + 1;
-            files{indx} = fullfile(path, tmp(iItem).name);
+            files{indx} = fullfile(inPath, tmp(iItem).name);
             filesize{indx} = tmp(iItem).bytes;
         end
     else
-        % if there is no matched items, go into the subdirectory
-        if depth > 1
-            [filesTmp, filesizeTmp] = listfile(fullfile(path, tmp(iItem).name), exppat, depth - 1);
+        % if there is no matched item, go into the subdirectory
+        if (depth > 1) && all(~ strcmpi(tmp(iItem).name, {'.', '..'}))
+            [filesTmp, filesizeTmp] = listfile(fullfile(inPath, tmp(iItem).name), exppat, depth - 1);
             files = cat(2, files, filesTmp);
             filesize = cat(2, filesize, filesizeTmp);
         end
