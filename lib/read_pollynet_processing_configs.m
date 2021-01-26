8ec8496ea1917d1a4a14e36b54a0e952772e7515
zhenping <zp.yin@whu.edu.cn>
2021-01-26 15:42:54 +0800

    Enhancement: Excel format for link file
    Enable to set Excel file `pollynet_processing_chain_link`, which is
    convenient for having an overview of all polly configs.
    
    Note:
    
    Old ASCII link file is also compatible.
    
    
    Former-commit-id: 59d2fd370f954c60b6c0f29becd316b5115abf44
    Former-commit-id: 36221e4e260991b7bf11321ca62e36a990d66d72
    Former-commit-id: b5e83709db405369fb18136293936934dbbf4555

--- lib/read_pollynet_processing_configs.m
+++ lib/read_pollynet_processing_configs.m
@@ -39,20 +39,42 @@ if exist(file, 'file') ~= 2
 end
 
 try
-    fid = fopen(file, 'r');
-    data = textscan(fid, '%s %s %s %s %s %s %s', 'Delimiter', ',', ...
-                    'Headerlines', 1);
+    [~, ~, fileExt] = fileparts(file);
 
-    for iRow = 1:length(data{1})
-        pollyConfigHistory.pollyVersion{iRow} = data{1}{iRow};
-        pollyConfigHistory.startTime = [pollyConfigHistory.startTime, ...
-            datenum(data{2}{iRow}, 'yyyy-mm-dd HH:MM:SS')];
-        pollyConfigHistory.endTime = [pollyConfigHistory.endTime, ...
-            datenum(data{3}{iRow}, 'yyyy-mm-dd HH:MM:SS')];
-        pollyConfigHistory.pollyConfigFile{iRow} = data{4}{iRow};
-        pollyConfigHistory.pollyProcessFunc{iRow} = data{5}{iRow};
-        pollyConfigHistory.pollyUpdateInfo{iRow} = data{6}{iRow};
-        pollyConfigHistory.pollyDefaultsFile{iRow} = data{7}{iRow};
+    if strcmpi(fileExt, '.txt')
+        % ASCII file
+        fid = fopen(file, 'r');
+        data = textscan(fid, '%s %s %s %s %s %s %s', 'Delimiter', ',', ...
+                        'Headerlines', 1);
+
+        for iRow = 1:length(data{1})
+            pollyConfigHistory.pollyVersion{iRow} = data{1}{iRow};
+            pollyConfigHistory.startTime = [pollyConfigHistory.startTime, ...
+                datenum(data{2}{iRow}, 'yyyy-mm-dd HH:MM:SS')];
+            pollyConfigHistory.endTime = [pollyConfigHistory.endTime, ...
+                datenum(data{3}{iRow}, 'yyyy-mm-dd HH:MM:SS')];
+            pollyConfigHistory.pollyConfigFile{iRow} = data{4}{iRow};
+            pollyConfigHistory.pollyProcessFunc{iRow} = data{5}{iRow};
+            pollyConfigHistory.pollyUpdateInfo{iRow} = data{6}{iRow};
+            pollyConfigHistory.pollyDefaultsFile{iRow} = data{7}{iRow};
+        end
+    elseif strcmp(fileExt, '.xlsx')
+        % Excel file
+        T = readtable(file, 'ReadRowNames', false, 'ReadVariableNames', false);
+
+        for iRow = 2:size(T, 1)
+            pollyConfigHistory.pollyVersion{iRow - 1} = T.Var1{iRow};
+            pollyConfigHistory.startTime = [pollyConfigHistory.startTime, ...
+                datenum(T.Var2{iRow}, 'yyyy-mm-dd HH:MM:SS')];
+            pollyConfigHistory.endTime = [pollyConfigHistory.endTime, ...
+                datenum(T.Var3{iRow}, 'yyyy-mm-dd HH:MM:SS')];
+            pollyConfigHistory.pollyConfigFile{iRow - 1} = T.Var4{iRow};
+            pollyConfigHistory.pollyProcessFunc{iRow - 1} = T.Var5{iRow};
+            pollyConfigHistory.pollyUpdateInfo{iRow - 1} = T.Var6{iRow};
+            pollyConfigHistory.pollyDefaultsFile{iRow - 1} = T.Var7{iRow};
+        end
+    else
+        error('Wrong file format for %s', file);
     end
 catch
     error('Failure in reading pollynet history configuration file.\n%s\n', file);
