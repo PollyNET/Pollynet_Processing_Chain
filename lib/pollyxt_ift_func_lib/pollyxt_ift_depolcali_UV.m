88e35b705803223cfb500c5610666380621fa035
zhenping <zp.yin@whu.edu.cn>
2021-02-05 23:06:48 +0800

    Improvement: exclude *analog* files
    Exclude *analog* files in data analysis (Only valid for PollyXT_IfT).
    
    
    Former-commit-id: 5f36f4e5fdeea3e15fb65bb02b3cb43d7299d61b
    Former-commit-id: 2f2cb9370d6a1edc1e749edb2a4a184b36ad48d3
    Former-commit-id: 8fdddd0b8ae37ad4692332873c8129b490b21ff6

--- lib/pollyxt_ift_func_lib/pollyxt_ift_depolcali_UV.m
+++ lib/pollyxt_ift_func_lib/pollyxt_ift_depolcali_UV.m
@@ -48,25 +48,29 @@ if config.flagMolDepolCali
         sig355Cro = squeeze(sum(data.signal(flagChannel355Cro, :, proIndx), 3));
         bg355Cro = squeeze(sum(data.bg(flagChannel355Cro, :, proIndx), 3));
 
-        refHIndx355 = data.refHIndx355(iGroup, 1):data.refHIndx355(iGroup, 2);
+        if ~ isnan(data.refHIndx355(iGroup, 1))
 
-        % molecular depolarization calibration
-        fprintf('Start molecular depolarization calibration for the %d cloud free period at 355 nm.\n', iGroup);
-        [this_depol_cal_fac_355, this_depol_cal_fac_std_355] = polly_molDepolCali(sig355Tot(refHIndx355), bg355Tot(refHIndx355), sig355Cro(refHIndx355), bg355Cro(refHIndx355), config.TR(flagChannel355Tot), 0, config.TR(flagChannel355Cro), 0, 10, defaults.molDepol355, defaults.molDepolStd355);
+            refHIndx355 = data.refHIndx355(iGroup, 1):data.refHIndx355(iGroup, 2);
 
-        depol_cal_fac_355 = cat(2, depol_cal_fac_355, this_depol_cal_fac_355);
-        depol_cal_fac_std_355 = cat(2, depol_cal_fac_std_355, this_depol_cal_fac_std_355);
-        depol_cal_start_time_355 = cat(2, depol_cal_start_time_355, data.mTime(proIndx(1)));
-        depol_cal_stop_time_355 = cat(2, depol_cal_stop_time_355, data.mTime(proIndx(end)));
+            % molecular depolarization calibration
+            fprintf('Start molecular depolarization calibration for the %d cloud free period at 355 nm.\n', iGroup);
+            [this_depol_cal_fac_355, this_depol_cal_fac_std_355] = polly_molDepolCali(sig355Tot(refHIndx355), bg355Tot(refHIndx355), sig355Cro(refHIndx355), bg355Cro(refHIndx355), config.TR(flagChannel355Tot), 0, config.TR(flagChannel355Cro), 0, 10, defaults.molDepol355, defaults.molDepolStd355);
+
+            depol_cal_fac_355 = cat(2, depol_cal_fac_355, this_depol_cal_fac_355);
+            depol_cal_fac_std_355 = cat(2, depol_cal_fac_std_355, this_depol_cal_fac_std_355);
+            depol_cal_start_time_355 = cat(2, depol_cal_start_time_355, data.mTime(proIndx(1)));
+            depol_cal_stop_time_355 = cat(2, depol_cal_stop_time_355, data.mTime(proIndx(end)));
+
+        end
 
     end
 
-    depCalAttri.depol_cal_fac_355 = depol_cal_fac_355;
-    depCalAttri.depol_cal_fac_std_355 = depol_cal_fac_std_355;
-    depCalAttri.depol_cal_start_time_355 = depol_cal_start_time_355;
-    depCalAttri.depol_cal_stop_time_355 = depol_cal_stop_time_355;
-
 end
+
+depCalAttri.depol_cal_fac_355 = depol_cal_fac_355;
+depCalAttri.depol_cal_fac_std_355 = depol_cal_fac_std_355;
+depCalAttri.depol_cal_start_time_355 = depol_cal_start_time_355;
+depCalAttri.depol_cal_stop_time_355 = depol_cal_stop_time_355;
 
 % select depolarization calibration factor
 [data.depol_cal_fac_355, data.depol_cal_fac_std_355, ...
