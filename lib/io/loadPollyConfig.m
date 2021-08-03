6e24872d896cf153648f00408e281a10c8b67c10
Zhenping <zp.yin@whu.edu.cn>
2021-08-03 15:20:48 +0800

    Enhancement: signal temperature correction
    Implement symbolic functions (string formulas) for signal temperature correction.
    
    "1": no temperature correction
    "exp(-0.001*T)": exponential correction function (Unit: Kelvin)
    
    This function needs to be specified for each channel individually and be configured
    in polly_config file.
    
    Associated with #92

--- lib/io/loadPollyConfig.m
+++ lib/io/loadPollyConfig.m
@@ -53,7 +53,6 @@ pollyGlobalConfig.is407nm = logical(pollyGlobalConfig.is407nm);
 pollyGlobalConfig.is607nm = logical(pollyGlobalConfig.is607nm);
 pollyGlobalConfig.isRR = logical(pollyGlobalConfig.isRR);
 pollyGlobalConfig.isParallel = logical(pollyGlobalConfig.isParallel);
-pollyGlobalConfig.flagRamanChannelTempCor = logical(pollyGlobalConfig.flagRamanChannelTempCor);
 
 if isfield(pollyConfig, 'isFR')
     pollyConfig.isFR = logical(pollyConfig.isFR);
@@ -114,11 +113,6 @@ if isfield(pollyConfig, 'isParallel')
     pollyConfig.isParallel = logical(pollyConfig.isParallel);
 else
     pollyConfig.isParallel = false(1, length(pollyConfig.is532nm));
-end
-if isfield(pollyConfig, 'flagRamanChannelTempCor')
-    pollyConfig.flagRamanChannelTempCor = logical(pollyConfig.flagRamanChannelTempCor);
-else
-    pollyConfig.flagRamanChannelTempCor = false(1, length(pollyConfig.is532nm));
 end
 
 %% overwrite polly global configs
