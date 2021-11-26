1ca762b53add82d655af672c09ac2b203c4ddc0f
ulysses78 <86650998+ulysses78@users.noreply.github.com>
2021-11-26 15:09:45 +0100

    Andi (#126)
    * added my_theme.css for changing width of html main-page and started to convert csv-tables to sphinx-tables in the config-section
    
    * contiuned to convert csv-tables to sphinx-tables in the config-section and rearanged csv-tables in terms of newlines
    
    * finished rearanging table widht in the polly configuration section

--- doc-auto/source/conf.py
+++ doc-auto/source/conf.py
@@ -78,6 +78,7 @@ exclude_patterns = []
 #
 #html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
+html_style = 'css/my_theme.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
