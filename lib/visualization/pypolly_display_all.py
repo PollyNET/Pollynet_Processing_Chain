c0634e815a443cab1864aca6ad8c6e23d3552b91
Baymax <test@email.com>
2024-08-21 06:30:15 +0000

    deactivated single profile plots, sep.poliphon-plots; checktime for single-files in concat-script;added minSig to global_config

--- lib/visualization/pypolly_display_all.py
+++ lib/visualization/pypolly_display_all.py
@@ -60,7 +60,7 @@ my_parser.add_argument('--outdir', dest='outdir', metavar='outputdir',
                        help='the output folder to put the png files to.')
 my_parser.add_argument('--retrieval', dest='retrieval', metavar='retrieval parameter',
                        default=['all'],
-                       choices=['all','attbsc','voldepol','cloudinfo','target_class','wvmr_rh','quasi_results','profiles','overlap','LC','HKD','longterm_cali','profile_summary'],
+                       choices=['all','attbsc','voldepol','cloudinfo','target_class','wvmr_rh','quasi_results','profiles','overlap','LC','HKD','longterm_cali','profile_summary','poliphon'],
                        nargs='+',
                        type=str,
                        help='the retrievals to be plotted; default: "all".')
@@ -302,48 +302,68 @@ def main():
              print("An error occurred:", e)
     
     
-    if ('all' in args.retrieval) or ('profiles' in args.retrieval):
+    if ('profiles' in args.retrieval):
         ## plotting profiles
         ## using profile_translator
 
-#        profile_translator = p_translator.profile_translator_function()
-#        NR_profile_translator = p_translator.NR_profile_translator_function()
-#        OC_profile_translator = p_translator.OC_profile_translator_function()
+        profile_translator = p_translator.profile_translator_function()
+        NR_profile_translator = p_translator.NR_profile_translator_function()
+        OC_profile_translator = p_translator.OC_profile_translator_function()
         POLIPHON_profile_translator = p_translator.POLIPHON_profile_translator_function()
 
         try:
-#            nc_profiles = readout.get_nc_filename(date, device, inputfolder, param='profiles')
-#            nc_profiles_NR = readout.get_nc_filename(date, device, inputfolder, param='NR_profiles')
-#            nc_profiles_OC = readout.get_nc_filename(date, device, inputfolder, param='OC_profiles')
+            nc_profiles = readout.get_nc_filename(date, device, inputfolder, param='profiles')
+            nc_profiles_NR = readout.get_nc_filename(date, device, inputfolder, param='NR_profiles')
+            nc_profiles_OC = readout.get_nc_filename(date, device, inputfolder, param='OC_profiles')
             nc_profiles_POLIPHON = readout.get_nc_filename(date, device, inputfolder, param='POLIPHON_1')
             print(f'plotting profiles to {outputfolder}')
-#            for profile in nc_profiles:
-#                nc_dict_profile = readout.read_nc_file(profile)
-#                starttime=datetime.utcfromtimestamp(int(nc_dict_profile['start_time'])).strftime('%H:%M')
-#                endtime=datetime.utcfromtimestamp(int(nc_dict_profile['end_time'])).strftime('%H:%M')
-#                print(f"profile: {starttime} - {endtime}")
-#                nc_dict_profile = readout.calc_ANGEXP(nc_dict_profile)
-#                for profilename in profile_translator.keys():
-#                    print(f"{profilename}")
-#                    display_profiles.pollyDisplay_profile(nc_dict_profile,profile_translator,profilename,config_dict,polly_conf_dict,outputfolder,donefilelist_dict=donefilelist_dict)
-#            for NR_profile in nc_profiles_NR:
-#                nc_dict_profile_NR = readout.read_nc_file(NR_profile)
-#                starttime=datetime.utcfromtimestamp(int(nc_dict_profile_NR['start_time'])).strftime('%H:%M')
-#                endtime=datetime.utcfromtimestamp(int(nc_dict_profile_NR['end_time'])).strftime('%H:%M')
-#                print(f"NR-profile: {starttime} - {endtime}")
-#                nc_dict_profile_NR = readout.calc_ANGEXP(nc_dict_profile_NR)
-#                for profilename in NR_profile_translator.keys():
-#                    print(f"{profilename}")
-#                    display_profiles.pollyDisplay_profile(nc_dict_profile_NR,NR_profile_translator,profilename,config_dict,polly_conf_dict,outputfolder,donefilelist_dict=donefilelist_dict)
-#            for OC_profile in nc_profiles_OC:
-#                nc_dict_profile_OC = readout.read_nc_file(OC_profile)
-#                starttime=datetime.utcfromtimestamp(int(nc_dict_profile_OC['start_time'])).strftime('%H:%M')
-#                endtime=datetime.utcfromtimestamp(int(nc_dict_profile_OC['end_time'])).strftime('%H:%M')
-#                print(f"OC-profile: {starttime} - {endtime}")
-#                nc_dict_profile_OC = readout.calc_ANGEXP(nc_dict_profile_OC)
-#                for profilename in OC_profile_translator.keys():
-#                    print(f"{profilename}")
-#                    display_profiles.pollyDisplay_profile(nc_dict_profile_OC,OC_profile_translator,profilename,config_dict,polly_conf_dict,outputfolder,donefilelist_dict=donefilelist_dict)
+            for profile in nc_profiles:
+                nc_dict_profile = readout.read_nc_file(profile)
+                starttime=datetime.utcfromtimestamp(int(nc_dict_profile['start_time'])).strftime('%H:%M')
+                endtime=datetime.utcfromtimestamp(int(nc_dict_profile['end_time'])).strftime('%H:%M')
+                print(f"profile: {starttime} - {endtime}")
+                nc_dict_profile = readout.calc_ANGEXP(nc_dict_profile)
+                for profilename in profile_translator.keys():
+                    print(f"{profilename}")
+                    display_profiles.pollyDisplay_profile(nc_dict_profile,profile_translator,profilename,config_dict,polly_conf_dict,outputfolder,donefilelist_dict=donefilelist_dict)
+            for NR_profile in nc_profiles_NR:
+                nc_dict_profile_NR = readout.read_nc_file(NR_profile)
+                starttime=datetime.utcfromtimestamp(int(nc_dict_profile_NR['start_time'])).strftime('%H:%M')
+                endtime=datetime.utcfromtimestamp(int(nc_dict_profile_NR['end_time'])).strftime('%H:%M')
+                print(f"NR-profile: {starttime} - {endtime}")
+                nc_dict_profile_NR = readout.calc_ANGEXP(nc_dict_profile_NR)
+                for profilename in NR_profile_translator.keys():
+                    print(f"{profilename}")
+                    display_profiles.pollyDisplay_profile(nc_dict_profile_NR,NR_profile_translator,profilename,config_dict,polly_conf_dict,outputfolder,donefilelist_dict=donefilelist_dict)
+            for OC_profile in nc_profiles_OC:
+                nc_dict_profile_OC = readout.read_nc_file(OC_profile)
+                starttime=datetime.utcfromtimestamp(int(nc_dict_profile_OC['start_time'])).strftime('%H:%M')
+                endtime=datetime.utcfromtimestamp(int(nc_dict_profile_OC['end_time'])).strftime('%H:%M')
+                print(f"OC-profile: {starttime} - {endtime}")
+                nc_dict_profile_OC = readout.calc_ANGEXP(nc_dict_profile_OC)
+                for profilename in OC_profile_translator.keys():
+                    print(f"{profilename}")
+                    display_profiles.pollyDisplay_profile(nc_dict_profile_OC,OC_profile_translator,profilename,config_dict,polly_conf_dict,outputfolder,donefilelist_dict=donefilelist_dict)
+            for POLIPHON in nc_profiles_POLIPHON:
+                nc_dict_profile_POLI = readout.read_nc_file(POLIPHON)
+                starttime=datetime.utcfromtimestamp(int(nc_dict_profile_POLI['start_time'])).strftime('%H:%M')
+                endtime=datetime.utcfromtimestamp(int(nc_dict_profile_POLI['end_time'])).strftime('%H:%M')
+                print(f"POLIPHON-profile: {starttime} - {endtime}")
+                for profilename in POLIPHON_profile_translator.keys():
+                    print(f"{profilename}")
+                    display_profiles.pollyDisplay_profile(nc_dict_profile_POLI,POLIPHON_profile_translator,profilename,config_dict,polly_conf_dict,outputfolder,donefilelist_dict=donefilelist_dict)
+        except Exception as e:
+             print("An error occurred:", e)
+    
+    if ('all' in args.retrieval) or ('poliphon' in args.retrieval):
+        ## plotting profiles
+        ## using profile_translator
+
+        POLIPHON_profile_translator = p_translator.POLIPHON_profile_translator_function()
+
+        try:
+            nc_profiles_POLIPHON = readout.get_nc_filename(date, device, inputfolder, param='POLIPHON_1')
+            print(f'plotting profiles to {outputfolder}')
             for POLIPHON in nc_profiles_POLIPHON:
                 nc_dict_profile_POLI = readout.read_nc_file(POLIPHON)
                 starttime=datetime.utcfromtimestamp(int(nc_dict_profile_POLI['start_time'])).strftime('%H:%M')
